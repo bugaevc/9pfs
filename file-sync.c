7d4811ec86ddbe9e976b97cd2e4ad593f59f1f7b
Sergey Bugaev <bugaevc@gmail.com>
2026-06-07 23:37:14 +0300

    Implement file_sync

--- file-sync.c
+++ file-sync.c
@@ -20,6 +20,7 @@
 #define _GNU_SOURCE 1
 
 #include "9pfs.h"
+#include "9p-rpc.h"
 #include "fs_S.h"
 #include <errno.h>
 
@@ -27,10 +28,32 @@ error_t
 S_file_sync (struct protid *pi, int wait, int omit_metadata)
 {
   error_t err;
+  struct p9_stat s;
 
   if (!pi)
     return EOPNOTSUPP;
+  if (p9_readonly)
+    /* Should we return EROFS?  */
+    return 0;
 
-  /* TODO */
-  return EROFS;
+
+  if (p9_version >= P9_VERSION_2000_L)
+    {
+      /* Ensure we're open at least for something.  */
+      if (!pi->server_open_flags)
+        {
+          err = p9_ensure_open (pi, O_READ);
+          if (err)
+            return err;
+        }
+      return p9_rpc (P9_FSYNC_REQUEST,
+                     "44", pi->io_fid, omit_metadata,
+                     "");
+    }
+
+  /* Use the special case of wstat.  */
+  p9_stat_dont_touch (&s);
+  err = p9_wstat (pi->walk_fid, &s);
+  p9_stat_free (&s);
+  return err;
 }
